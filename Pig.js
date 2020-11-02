2bcc0348b1068f031119305b02c810b5da507e98
arjun20116969 <72213942+arjun20116969@users.noreply.github.com>
2020-11-02 10:19:59 -0500

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,6 @@
+class Pig extends BaeClass{
+ constructor(x,y,width,height){
+   super(x,y,50,50);
+   this.image=loadImage("sprites/enemy.png");
+ }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Angy-Birds
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
